feaf9a301ef1d99a8fcce516630eccd1c2819370
donatociao <ciao.donatociao@gmail.com>
2019-05-21 16:42:17 +0200

    WOW :)

--- main.js
+++ main.js
@@ -8,58 +8,45 @@
 // quando le chiamate ajax funzionano e il codice fa tutto quello che deve fare
 // (cambiare colore al riquadro e inserire il numero), allora potete generare la griglia con jQuery,
 // se vi fa piacere
- $(document).ready(function() {
+$(document).ready(function() {
+  for (var i = 0; i < 36; i++) {
+    $.ajax({
+      url: "https://www.boolean.careers/api/random/int",
+      method: 'GET',
+      success: function(data) {
+        //salvo il numero della API in una variabile
+        var random = data.response;
+        //salvo il template del quadrato in una variabile
+        var source = $('#square-gen').html();
+        //compilo il template
+        var template = Handlebars.compile(source);
+        //imposto variabile in caso di vittoria
+        var win = {
+          'gameclass': "win"
+        };
+        //imposto variabile in caso di sconfitta
+        var lose = {
+          'gameclass': 'lose'
+        }
+        //imposto istruzioni di compilazione e compilo
+        var quadrato;
 
-  $.ajax({
-    url: "https://www.boolean.careers/api/random/int",
-    method: 'GET',
-    success: function(data) {
-      //salvo il numero della API in una variabile
-      var random = data.response;
-      console.log('il response API è' + data.response);
-
-      //salvo il template del quadrato in una variabile
-      var source = $('#square-gen').html();
-
-      //compilo il template
-      var template = Handlebars.compile(source);
-      console.log(source);
-
-      //imposto variabile in caso di vittoria
-      var win = {
-        'gameclass': "win"
-      };
-
-      //imposto variabile in caso di sconfitta
-      var lose = {
-        'gameclass': 'lose'
-      }
-
-      //imposto istruzioni di compilazione
-      var quadrato;
-      if (random <= 5) {
-        quadrato = template(win);
-      } else {
-        quadrato = template(lose);
-      }
-
-      //inserisco template generato nel body
-      $('.game').append(quadrato);
-    },
-    error: function() {
-      alert('errore')
+        if (random <= 5) {
+          quadrato = template(win);
+        } else {
+          quadrato = template(lose);
+        }
+        //inserisco template generato nel body
+        $('.game').append(quadrato);
+      },
+      error: function() {
+        alert('errore')
+    }
+    });
   }
-  });
-
-  //compilo template con handlebars
-
-
 });
 
 
-// //popolo il contenitore con i quadrati da cliccare
-// var i = 0;
-// while (i = 35) {
-//   $('.game').append($('.square').clone());
-//   i++;
-//  }
+$(document).on('click','.square', function() {
+  $(this).toggleClass('click');
+});
